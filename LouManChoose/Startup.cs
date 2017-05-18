450d78bae60753b4697a8b81b37a0deb5a71bfdc
Lou Lee <Lou7532@hotmail.com>
2017-05-18 14:15:38 -0400

    first commit

---
+++ LouManChoose/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LouManChoose.Startup))]
+namespace LouManChoose
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
